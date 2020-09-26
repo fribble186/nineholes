2d78e78b6d7f8aeb91e9b7cab6c5dcf608b15448
fribble186 <psj124@qq.com>
2020-09-26 08:11:28 +0800

    update

--- backend/main.py
+++ backend/main.py
@@ -61,7 +61,7 @@ async def websocket_endpoint(ws: WebSocket, room: str):
 if __name__ == "__main__":
     import uvicorn
     # 官方推荐是用命令后启动 uvicorn main:app --host=127.0.0.1 --port=8010 --reload
-    uvicorn.run(app='main:app', host="127.0.0.1", port=8010, reload=True, debug=True)
+    uvicorn.run(app='main:app', host="0.0.0.0", port=8010, reload=True, debug=True)
 
 
 
